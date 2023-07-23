3916b75ca6e921763b66c661d4826837851fd4de
Justin McCarty <justin.d.mccarty@gmail.com>
2023-07-22 19:31:27 -0700

    Update streamlit_app_1.py

--- streamlit_app_1.py
+++ streamlit_app_1.py
@@ -1,1 +1,23 @@
+import streamlit as st
+import graphviz as gv
+
+# Create a Digraph object
+flowchart = gv.Digraph()
+
+# Add nodes
+flowchart.node('A', 'Step 1')
+flowchart.node('B', 'Step 2')
+flowchart.node('C', 'Step 3')
+flowchart.node('D', 'Step 4')
+flowchart.node('E', 'Step 5')
+
+# Add edges
+flowchart.edges(['AB', 'BC', 'CD', 'DE'])
+
+# Save the flowchart as a png image
+flowchart.format = 'png'
+flowchart.render('flowchart')
+
+# Display the flowchart image in Streamlit
+st.image('flowchart.png')
 
