3c50cef1c409ab18485f56a11f11633564a4bb9a
Justin McCarty <justin.d.mccarty@gmail.com>
2023-07-22 18:00:07 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,15 @@
+# Import the streamlit library
+import streamlit as st
+
+# Title of the web app
+st.title('My first Streamlit app')
+
+# Ask the user for their name
+name = st.text_input('What is your name?')
+
+# Display a welcome message
+if name:
+    st.write(f'Hello, {name}! Welcome to the app!')
+else:
+    st.write('Please enter your name.')
 
