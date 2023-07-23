80c2e8e5ec04558e354e3aef86fced81c1d701ac
Justin McCarty <justin.d.mccarty@gmail.com>
2023-07-22 18:50:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,7 @@ df = pd.DataFrame(
         "name": ["Roadmap", "Extras", "Issues"],
         "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
         "stars": [random.randint(0, 5) for _ in range(3)],
-        "views_history": [[random.randint(0, 30) for _ in range(30)] for _ in range(3)],
-    }
+        "sales": [200, 550, 1000]    }
 )
 st.dataframe(
     df,
@@ -20,9 +19,6 @@ st.dataframe(
             format="%d ⭐",
         ),
         "url": st.column_config.LinkColumn("App URL"),
-        "shipping time": st.column_config.BarChartColumn(
-            "Shipping Days", y_min=0, y_max=30
-        ),
         "sales": st.column_config.ProgressColumn(
             "Sales volume",
             help="The sales volume in USD",
