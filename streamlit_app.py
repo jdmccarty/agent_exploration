637757be7ba3338d3ffecca0682ffd447028ac87
Justin McCarty <justin.d.mccarty@gmail.com>
2023-07-22 19:17:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,29 +2,59 @@ import random
 import pandas as pd
 import streamlit as st
 
-df = pd.DataFrame(
-    {
-        "name": ["Roadmap", "Extras", "Issues"],
-        "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
-        "stars": [random.randint(0, 5) for _ in range(3)],
-        "sales": [200, 550, 1000]    }
-)
+data = {
+    "product_name": [
+        "Commercial Espresso Machine", 
+        "Commercial Espresso Machine"
+    ],
+    "supplier_name": [
+        "Espresso Parts", 
+        "Espresso Zone"
+    ],
+    "address": [
+        "4315 Lacey Blvd. SE Lacey, WA 98503", 
+        "6911 Washington Ave, Philadelphia, PA 19142"
+    ],
+    "price": [
+        "$5000-$8000", 
+        "$2000-$15000"
+    ],
+    "quality": [
+        "Medium", 
+        "High"
+    ],
+    "environmental_score": [
+        2, 
+        1
+    ],
+    "ETA": [
+        "750 miles",
+        "2900 miles"
+    ]
+}
+
+# Create a pandas DataFrame
+df = pd.DataFrame(data)
+
 st.dataframe(
     df,
     column_config={
-        "name": "Supplier Name",
-        "stars": st.column_config.NumberColumn(
-            "Product Rateing",
-            help="Number of Review Stars",
+        "product_name": "Product Name",
+        "supplier_name": "Supplier Name",
+        "address": "Address",
+        "price": "Price",
+        "quality": "Quality",
+        "environmental_score": st.column_config.NumberColumn(
+            "Environmental Score",
+            help="Stars",
             format="%d ⭐",
         ),
-        "url": st.column_config.LinkColumn("App URL"),
-        "sales": st.column_config.ProgressColumn(
-            "Sales volume",
-            help="The sales volume in USD",
+        "ETA": st.column_config.ProgressColumn(
+            "ETA",
+            help="ETA",
             format="$%f",
             min_value=0,
-            max_value=1000
+            max_value=3000
         ),
     },
     hide_index=True,
