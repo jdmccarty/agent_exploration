ceccf578421bde37f50dc1f24210bce9f95c3f44
Justin McCarty <justin.d.mccarty@gmail.com>
2023-07-22 18:41:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ df = pd.DataFrame(
     {
         "name": ["Roadmap", "Extras", "Issues"],
         "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
-        "stars": [random.randint(0, 1000) for _ in range(3)],
-        "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
+        "stars": [random.randint(0, 5) for _ in range(3)],
+        "views_history": [[random.randint(0, 30) for _ in range(30)] for _ in range(3)],
     }
 )
 st.dataframe(
